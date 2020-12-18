0404dca885fdc0496cccf3ff88ba2dd7c157ba44
Vannisa Pascal-Nelson <vannisaepascalnelson@gmail.com>
2020-12-18 01:08:49 -0500

    post-to-db

--- src/models/model.js
+++ src/models/model.js
@@ -12,6 +12,16 @@ class Model {
     if (clause) query += clause;
     return this.pool.query(query);
   }
+  async insertWithReturn(columns, values) {
+    const query = `
+          INSERT INTO ${this.table}(${columns})
+          VALUES (${values})
+          RETURNING id, ${columns}
+      `;
+    return this.pool.query(query);
+  }
+
+
 }
 
 export default Model;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { indexPage, funPage} from '../controllers';
-import {messagesPage} from '../controllers/messages';
+import { indexPage,  funPage} from '../controllers';
+import {messagesPage, addMessage} from '../controllers/messages';
 const indexRouter = express.Router();
 
 indexRouter.get('/', indexPage);
 indexRouter.get('/fun', funPage);
 indexRouter.get('/messages', messagesPage);
+indexRouter.post('/messages',addMessage);
 
 export default indexRouter;
