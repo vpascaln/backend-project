cac6a1470a4a1274d928787cdc06bf8accecda42
Vannisa Pascal-Nelson <vannisaepascalnelson@gmail.com>
2020-12-17 23:43:41 -0500

    connect-postgres

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
 import express from 'express';
-import { indexPage, funPage } from '../controllers';
+import { indexPage, funPage} from '../controllers';
+import {messagesPage} from '../controllers/messages';
 const indexRouter = express.Router();
 
 indexRouter.get('/', indexPage);
 indexRouter.get('/fun', funPage);
+indexRouter.get('/messages', messagesPage);
 
 export default indexRouter;
---
+++ src/utils/queries.js
@@ -0,0 +1,17 @@
+export const createMessageTable = `
+DROP TABLE IF EXISTS messages;
+CREATE TABLE IF NOT EXISTS messages (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR DEFAULT '',
+  message VARCHAR NOT NULL
+  )
+  `;
+
+export const insertMessages = `
+INSERT INTO messages(name, message)
+VALUES ('chidimo', 'first message'),
+      ('orji', 'second message'), 
+      ('lala', 'third message')
+`;
+
+export const dropMessagesTable = 'DROP TABLE messages';
